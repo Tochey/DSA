c34870d55e28f91ed067b7962d1a72f1dc7649d2
DON <don@DONs-Laptop-9.local>
2023-01-31 01:43:41 -0500

    init

---
+++ strings/e/LongestCommonPrefix.java
@@ -0,0 +1,19 @@
+package strings.e;
+
+// https://leetcode.com/problems/longest-common-prefix/
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+
+        String prefix = strs[0];
+        for (int index = 1; index < strs.length; index++) {
+            while (strs[index].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+
+    //TODO: implement b-seach and divide and conquer
+}
