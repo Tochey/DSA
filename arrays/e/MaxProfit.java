de3931f02e042e0dba90db0dc3c7189a786ba12d
DON <don@DONs-Laptop-9.local>
2023-02-06 00:16:37 -0500

    few more easy

--- arrays/e/MaxProfit.java
+++ arrays/e/MaxProfit.java
@@ -3,8 +3,16 @@ package arrays.e;
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 public class MaxProfit {
     public int maxProfit(int[] prices) {
-        
+        int mp = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = 1; j < prices.length; j++) {
+                int currPrice = prices[j] - prices[i];
+                if (currPrice > 0 && currPrice > mp && i < j) {
+                    mp = currPrice;
+                }
+            }
+        }
 
-        return 0;
+        return mp;
     }
 }
